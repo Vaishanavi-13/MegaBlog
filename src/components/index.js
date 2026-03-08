79e027061654a2e44e179f9939257a566e285cdf
Vaishanavi-13 <vaishanavipatukale96@gmail.com>
2026-03-08 11:40:01 +0530

    All pages are created and added to the index.js file. The RTE component is also created and added to the index.js file.

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,36 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-
+import RTE from "./RTE";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import PostForm from "./PostForm";
+import Home from "./pages/Home";
+import AllPosts from "./pages/AllPosts";
+import AddPost from "./pages/AddPost";
+import EditPost from "./pages/EditPost";    
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Signup from "./pages/Signup";
+import Post from "./pages/Post";
 
 export {
     Header,
     Container,
     Footer,
     Logo,
-    LogoutBtn
-    
+    LogoutBtn,
+    RTE,
+    Select,
+    Home,
+    AllPosts,
+    AddPost,
+    EditPost,
+    PostCard,
+    PostForm,   
+    Login,
+    Register,
+    Signup,
+    Post
+
 }
